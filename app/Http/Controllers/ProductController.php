04fc4d98b0abc6da6fe2b34f3503070574920e1c
allysafgorduiz9-lab <allysafgorduiz9@gmail.com>
2026-05-21 22:20:11 +0800

    auto-commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display the manage menu / products page.
+     */
+    public function index()
+    {
+        // For now, we will just return a simple blank view. 
+        // You can pass your products database data here later!
+        return view('admin.products.index');
+    }
 }
