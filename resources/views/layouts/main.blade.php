0461319e4dcd68a046828e78bc4073603c9abb95
allysafgorduiz9-lab <allysafgorduiz9@gmail.com>
2026-05-21 14:54:37 +0800

    commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,7 +33,21 @@
                 <a href="/admin" title="Staff Login" class="text-gray-400 hover:text-white">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
                 </a>
-                <a href="/cart" class="bg-yellow-600 hover:bg-yellow-700 px-5 py-2 rounded font-bold transition">Cart 🛒</a>
+                @php
+    $storeStatus = session('store_status', 'open');
+@endphp
+
+@if($storeStatus == 'open')
+    <a href="/cart"
+       class="bg-yellow-600 hover:bg-yellow-700 px-5 py-2 rounded font-bold transition">
+       Cart 🛒
+    </a>
+@else
+    <button
+       class="bg-gray-500 cursor-not-allowed px-5 py-2 rounded font-bold text-white">
+       Store Closed
+    </button>
+@endif
             </div>
         </div>
     </nav>
